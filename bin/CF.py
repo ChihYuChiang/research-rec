9daf66285b7478208d58d9c7b57de22262031bd4
ChihYuChiang <ckccyu@gmail.com>
2018-03-20 15:02:51 -0500

    Update: CF basic

--- bin/CF.py
+++ bin/CF.py
@@ -31,33 +31,57 @@ temp = [np.take(naniloc_inv[1], np.where(naniloc_inv[0] == i)).flatten() for i i
 #Get specific rating: [Rater][game]
 pref_nan[0][3]
 
+#Total num of rating
+nMN = len(naniloc_inv[0])
+
 
 #--Leave-one-out cross validation
-#Select games as the test target
-[np.random.choice(temp[i]) for i in np.arange(nM)]
+# for i in np.arange(nM):
+#     for j in np.arange(nN):
+#         item_test = temp[i]
+
+#Select test target
+temp[0][0]
+
+#Mask the pref_nan to acquire the training data
+pref_train = pref_nan.copy()
+pref_train[0, temp[0][0]] = np.nan
 
 
 #--Column mean imputation
 #Compute column mean
-nMean = np.nanmean(pref_nan, axis=0)
+nMean = np.nanmean(pref_train, axis=0)
 
 #Find nan iloc
-naniloc = np.where(np.isnan(pref_nan))
+naniloc = np.where(np.isnan(pref_train))
 
 #Insert appropriate value into the matrix where is nan
 #np.take is faster than fancy indexing. e.g. nMean[[1, 3, 5]]
-pref = pref_nan.copy()
-pref[naniloc] = np.take(nMean, naniloc[1])
+pref_train[naniloc] = np.take(nMean, naniloc[1])
 
 
 #--SVD
 #Complete SVD
-u, s, vh = np.linalg.svd(pref)
+u, s, vh = np.linalg.svd(pref_train)
 print(u.shape, s.shape, vh.shape)
 
 #Truncated SVD (use t largest singular values)
 u_t1 = u[:, 0:20]
 u_t2 = u[:, 1:21]
 
+
+#--Distance matrix
 u_dist1 = squareform(pdist(u_t1, 'cosine'))
 u_dist1[0, :]
+
+#Sort, remove self, and find the best matched raters
+matched = np.delete(np.argsort(u_dist1[0, :]), 0)
+reference = []
+for rater in matched:
+    if np.isnan(pref_nan[rater, temp[0][0]]): continue
+    if len(reference) == 10: break
+    reference.append(pref_nan[rater, temp[0][0]])
+
+np.median(reference)
+pref_nan[0][3]
+nMean[3]
